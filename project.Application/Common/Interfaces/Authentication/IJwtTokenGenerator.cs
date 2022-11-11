801019fb7acefcb10a49b1e5ded545c20ec24b67
hakim <hakim_yahi@hotmail.fr>
2022-11-11 12:26:09 +0100

    Add Common Classes

---
+++ project.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace project.Application.Common.Interfaces.Authentication;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(Guid userID, string firstName, string lastName);
+}
+
