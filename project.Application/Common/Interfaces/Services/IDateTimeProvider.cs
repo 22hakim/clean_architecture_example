218669e84bbea1f5b7f44f443996cc1d475e8cc8
hakim <hakim_yahi@hotmail.fr>
2022-11-30 23:16:18 +0100

    Add dotnet user secrets key

---
+++ project.Application/Common/Interfaces/Services/IDateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace project.Application.Common.Interfaces.Services;
+
+public interface IDateTimeProvider
+{
+    // les deux sont des possibilités  
+    DateTime Now { get; }
+    //DateTimeOffset Now { get; }
+}
+
+
+
+
+
+
---
+++ project.Infra/Services/DateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using project.Application.Common.Interfaces.Services;
+
+namespace project.Infra.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime Now => DateTime.Now;
+}
+
+ 
