80b50f78eca34d468e48d38db214f4107fe00a78
hakim <hakim_yahi@hotmail.fr>
2023-01-28 17:33:50 +0100

    Add Filter errorHandling

---
+++ web_api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace project.api.Filters;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception; // équivalent Ex dans middleware 
+
+        context.Result = new ObjectResult(new { error = "Une erreur géré par filter" })
+        {
+            StatusCode = 500
+        };
+
+        context.ExceptionHandled = true;
+    }
+}
+
